3bbd7a71b627c93319999f1f0939dd3d71811eca
Mauricio Bouabci <maubouabci@gmail.com>
2013-07-13 21:34:17 +0000

    correction to file

--- grader.js
+++ grader.js
@@ -21,14 +21,13 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 
 */
-
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
-var assetFileExists = function(infile) {
+var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
